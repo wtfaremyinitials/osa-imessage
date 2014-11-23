8ff5fdb213511a897275b67bc3b611c49d2b3157
Will Franzen <wtfranzen@gmail.com>
2014-11-23 14:49:31 -0600

    Start on receiving messages. Creating files works, but the script isn't created properly

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+var fs = require('fs');
 var osa = require('osa');
+var uuid = require('node-uuid').v1();
+var mkfifo = require('mkfifo').mkfifoSync;
+var execSync = require('exec-sync');
 
 var PHONE_REGEX = /'^\+\d{10}$'/;
 var EMAIL_REGEX = /^\S+@\S+$/; // simple email regex
+
+var user = execSync('whoami');
+
+var SCRIPT_PATH = '/Users/' + user + '/Library/Application Scripts/com.apple.iChat/osa-imessage-' + uuid + '.scpt';
+var FIFO_PATH   = '/tmp/osa-imessage-' + uuid + '.fifo';
+
+var listenScript = (fs.readFileSync('./lib/events.scpt')+'');
 
 var noop = function(){};
 
@@ -49,6 +60,22 @@ iMessage.getContact = function(input, cb) {
             handle: found.handle()
         };
     }, search, cb);
+};
+
+iMessage.listen = function() {
+    mkfifo(FIFO_PATH, 0755);
+
+    var listener = listenScript.replace('%path%', FIFO_PATH);
+    var fd = fs.openSync(SCRIPT_PATH, 'w');
+    fs.writeSync(fd, listener);
+
+    // register with Messages.app
+};
+
+iMessage.unlisten = function() {
+    // unregister with Messages.app
+    fs.unlinkSync(FIFO_PATH);
+    fs.unlinkSync(SCRIPT_PATH);
 };
 
 module.exports = iMessage;
