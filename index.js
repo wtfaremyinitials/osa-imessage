b0917533f3ae8aa9a256c589e84c408bde262c40
Will Franzen <wtfranzen@gmail.com>
2014-12-10 23:52:11 -0600

    Fix file path issue

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var user = execSync('whoami');
 var SCRIPT_PATH = '/Users/' + user + '/Library/Application Scripts/com.apple.iChat/osa-imessage-' + uuid + '.scpt';
 var FIFO_PATH   = '/tmp/osa-imessage-' + uuid + '.fifo';
 
-var listenScript = fs.readFileSync('./lib/events.scpt');
+var listenScript = fs.readFileSync(__dirname + '/lib/events.scpt');
 
 var len = 108-72;
 for(var i=0; i<len; i++)
