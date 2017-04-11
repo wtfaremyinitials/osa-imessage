c0d4fc5a337766e7c5ef844f86f63fabea98914c
Will Franzen <wtfranzen@gmail.com>
2017-04-10 20:01:08 -0500

    Complete rewrite
    Notable changes
     - Reintroduce receive functionality (more robust than before)
     - Split `send(name, message)` into `send(handle, message)` and
       `handleForName(name)`
     - Promise based API

--- index.js
+++ index.js
@@ -1,57 +1,104 @@
 var fs = require('fs')
-var osa = require('osa')
-var spawn = require('child_process').spawn
+var osa = require('osa2')
 
-var EventEmitter = require("events").EventEmitter
-var LineStream = require('byline').LineStream
+// TODO: Check macOS version
 
-var PHONE_REGEX = /^\+\d{3,}$/
-var EMAIL_REGEX = /^\S+@\S+$/
+// Instead of doing something reasonable, Apple stores dates as the number of
+// seconds since 01-01-2001 00:00:00 GMT. DATE_OFFSET is the offset in seconds
+// between their epoch and unix time
+var DATE_OFFSET = 978307200
 
-var parse = function(input) {
-    if(typeof(input) == 'undefined')
-        return ''
-    else if(typeof(input) == 'object')
-        return input
-    else if(input.match(PHONE_REGEX) || input.match(EMAIL_REGEX))
-        return { handle: input }
-    else
-        return { name: input }
-    return ''
+// Gets the current Apple-style timestamp
+function appleTimeNow() {
+    return Math.floor(Date.now() / 1000) - DATE_OFFSET
 }
 
-var iMessage = {}
+// Transforms an Apple-style timestamp to a proper unix timestamp
+function fromAppleTime(at) {
+    if (at == 0) {
+        return null
+    }
+    return new Date((at + DATE_OFFSET) * 1000)
+}
 
-iMessage.send = function(message, to, cb) {
-    var recipient = parse(to)
-    cb = cb || function(){}
+// Attempt to require in optional dep sqlite with a more helpful error message
+function requireSqlite() {
+    try {
+        return require('sqlite3')
+    } catch (e) {
+        throw new Error('sqlite3 optional dependency required to receive messages')
+    }
+}
 
-    osa(function(message, recipient) {
+// Gets the proper handle string for a contact with the given name
+function handleForName(name) {
+    return osa((name) => {
         var Messages = Application('Messages')
-        recipient = Messages.buddies.whose(recipient)[0]
+        return Messages.buddies.whose({ name: name })[0].handle()
+    })(name)
+} 
 
-        Messages.send(message, { to: recipient })
-
-        return {
-            name:   recipient.name(),
-            handle: recipient.handle()
-        }
-    }, message, recipient, cb)
+// Sends a message to the given handle
+function send(handle, message) {
+    return osa((handle, message) => {
+        var Messages = Application('Messages')
+        var buddy = Messages.buddies.whose({ handle: handle })[0]
+        Messages.send(message, { to: buddy })
+    })(handle, message)
 }
 
-iMessage.getContact = function(input, cb) {
-    var search = parse(input)
-    cb = cb || function(){}
+var emitter = null
+function listen() {
+    // If listen has already been run, return the existing emitter
+    if (emitter != null) {
+        return emitter
+    }
 
-    osa(function(search) {
-        var Messages = Application('Messages');
-        found = Messages.buddies.whose(search)[0];
+    // Create an EventEmitter
+    var emitter = new (require('events').EventEmitter)()
 
-        return {
-            name:   found.name(),
-            handle: found.handle()
-        }
-    }, search, cb)
+    // Set up the database
+    var sqlite = requireSqlite() 
+    var db = new sqlite.Database(
+        process.env.HOME + '/Library/Messages/chat.db',
+        sqlite.OPEN_READONLY
+    )
+
+    var last = appleTimeNow()
+
+    var intv = setInterval(() => {
+        var query = `
+            SELECT guid, id as handle, text, date, date_read, is_from_me
+            FROM message
+            JOIN handle ON message.handle_id = handle.ROWID
+            WHERE date >= ${last}
+        `
+
+        db.each(query, (err, row) => {
+            if (err) {
+                clearInterval(intv)
+                emitter.emit('error', err)
+                console.error([
+                    'sqlite3 returned an error while polling for new message!',
+                    'bailing out of poll routine for safety. new messages will',
+                    'not be detected'
+                ].join('\n'))
+            }
+
+            emitter.emit('message', {
+                guid: row.guid,
+                text: row.text,
+                handle: row.handle,
+                fromMe: !!row.is_from_me,
+                date: fromAppleTime(row.date),
+                dateRead: fromAppleTime(row.date_read)
+            })  
+        })
+
+        last = appleTimeNow()
+    }, 1000)
+
+    return emitter
 }
 
-module.exports = iMessage
+module.exports = { send, listen, handleForName }
