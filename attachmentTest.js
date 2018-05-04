98979e3ac328a8fe21eb8c2bf981d428c2bc1c21
Elliot Plant <elliotaplant@gmail.com>
2018-05-04 14:03:37 -0700

    Remove test file

--- attachmentTest.js
+++
@@ -1,6 +0,0 @@
-const {send, sendFile} = require('./index');
-
-// send('+15109175552', 'Hullo Wurld');
-
-sendFile('+15109175552', '/Users/elliotaplant/Desktop/Nicole.mov')
-  .catch(console.error);
